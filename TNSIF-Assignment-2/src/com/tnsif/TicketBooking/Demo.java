ff117da3062f0931d44bdd2bfe5254896360876f
sandhyaasingh <saandhyasingh17@gmail.com>
2025-04-02 00:00:08 +0530

    commit

---
+++ TNSIF-Assignment-2/src/com/tnsif/TicketBooking/Demo.java
@@ -0,0 +1,22 @@
+package com.tnsif.TicketBooking;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        TicketBooking person1 = new TicketBooking();
+        TicketBooking person2 = new TicketBooking();
+        TicketBooking person3 = new TicketBooking();
+        TicketBooking person4 = new TicketBooking();
+        TicketBooking person5 = new TicketBooking();
+
+        TicketBooking.displayTickets();
+
+        person1.bookTicket();
+        person2.bookTicket();
+        person3.bookTicket();
+        person4.bookTicket();
+        person5.bookTicket();
+
+        TicketBooking.displayTickets();
+    }
+}
---
+++ TNSIF-Assignment-2/src/com/tnsif/TicketBooking/TicketBooking.java
@@ -0,0 +1,21 @@
+package com.tnsif.TicketBooking;
+
+public class TicketBooking {
+
+    private static int tickets = 5;
+
+    public static void displayTickets() {
+        System.out.println("Total tickets available: " + tickets);
+    }
+
+    public void bookTicket() {
+        if (tickets > 0) {
+            tickets--;
+            System.out.println("Ticket booked successfully!");
+            System.out.println("Tickets left: " + tickets);
+        } else {
+            System.out.println("No tickets available for booking.");
+        }
+    }
+}
+
