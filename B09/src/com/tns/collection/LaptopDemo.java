0c71448d5e6b4e64ecb2bf1b2c05530d4351d9b0
sandhyaasingh <saandhyasingh17@gmail.com>
2025-04-09 17:38:11 +0530

    commit

---
+++ B09/src/com/tns/collection/LaptopDemo.java
@@ -0,0 +1,24 @@
+package com.tns.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LaptopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Laptop> laps = new ArrayList<Laptop>();
+		
+		laps.add(new Laptop("dell", 15, 169999));
+		laps.add(new Laptop("asus", 22, 17787));
+		laps.add(new Laptop("apple", 2, 177878));
+		
+Collections.sort(laps);
+for(Laptop l: laps)
+{
+	System.out.println(l);
+}
+	}
+
+}
